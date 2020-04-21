c6e4778fdde8d932e456b4dde8bd38a1a199e320
Rakshit Chhodavadia <43170545+rakshit33@users.noreply.github.com>
2020-04-21 19:19:29 -0400

    Final

--- js/news.js
+++ js/news.js
@@ -1,4 +1,4 @@
-var keywords = 'coronavirus, covid-19'
+var keywords = 'coronavirus'
 var date = new Date();
 var today = date.getFullYear() +'-'+String(date.getMonth() + 1).padStart(2, '0')+'-'+String(date.getDate()).padStart(2, '0');
 
@@ -11,12 +11,20 @@ var url = 'http://newsapi.org/v2/everything?' +
 $.getJSON(url,function(data){
     
     var html = '';
+    var url, title, author, urlToImage, description = ' ';
     $(".news-area").html(html);
     for(i in data.articles){
-        html += '<div class="news-card"><div class="container"><a target="_blank" href="'+data.articles[i].url+'"><div class="news-title"><h2>'+data.articles[i].title+'</h2></div></a>';
-        html += '<div class="news-author">'+data.articles[i].author+'</div>';
-        html += '<div class="news-image"><img src="'+data.articles[i].urlToImage+'" alt="News Image"></div>';
-        html += '<div class="news-description">'+data.articles[i].description+'</div></div></div>';
+        
+        url = data.articles[i].url;
+        title = data.articles[i].title;
+        author = data.articles[i].author;
+        urlToImage = data.articles[i].urlToImage;
+        description = data.articles[i].description;
+        
+        html += '<div class="news-card"><div class="container"><a target="_blank" href="'+url+'"><div class="news-title"><h2>'+title+'</h2></div></a>';
+        html += '<div class="news-author">'+author+'</div>';
+        html += '<div class="news-image"><img src="'+urlToImage+'" alt="News Image"></div>';
+        html += '<div class="news-description">'+description+'</div></div></div>';
     }
     $(".news-area").html(html);
     //console.log(today);
